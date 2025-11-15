b89859cd7a944b9aaabbc222aa0c807b2c2884a3
Andresoto <zuluandres27@gmail.com>
2025-11-15 17:12:54 -0500

    init project

---
+++ src/app/incoming-exit/incoming-exit.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-incoming-exit',
+  imports: [],
+  templateUrl: './incoming-exit.html',
+  styleUrl: './incoming-exit.scss',
+})
+export class IncomingExit {
+
+}
